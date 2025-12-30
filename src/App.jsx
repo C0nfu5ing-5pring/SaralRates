3a25b973c63097847b3d378741c5f911be82f805
Shish <shishfruitwala29@gmail.com>
2025-12-30 16:14:30 +0530

    project setup and initial commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Home from "./components/Home";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
+const App = () => {
+  return (
+    <div>
+      <Header />
+      <Home />
+      <Footer />
+    </div>
+  );
+};
+
+export default App;
