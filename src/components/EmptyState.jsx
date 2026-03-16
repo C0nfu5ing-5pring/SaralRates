efa98a63b7b9a8f6cd2e83d014efc88f70c53efa
Shish <shishfruitwala29@gmail.com>
2026-03-16 17:20:53 +0530

    docs: remove code that relable on old variable

--- src/components/EmptyState.jsx
+++ src/components/EmptyState.jsx
@@ -1,15 +1,13 @@
 import { TriangleAlert } from "lucide-react";
 
-export default function EmptyState({ view, hasPriceHistory }) {
+export default function EmptyState({ view }) {
   return (
     <div className="flex flex-col justify-center h-[80vh] items-center">
       <TriangleAlert size={120} style={{ color: "gray" }} />
       <p className="text-gray-500 text-center">
         {view === "favourites"
           ? "No favourites added yet"
-          : hasPriceHistory
-            ? "No matching records"
-            : "Price history not available yet"}
+          : "No matching records"}
       </p>
     </div>
   );
