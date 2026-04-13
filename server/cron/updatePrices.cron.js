7e7805551816680c21f6a5619350cba6ec76e0bf
Shish <shishfruitwala29@gmail.com>
2026-04-13 14:46:25 +0530

    feat(api): daily fetch

--- server/cron/updatePrices.cron.js
+++ server/cron/updatePrices.cron.js
@@ -5,7 +5,7 @@ let pollInterval = null;
 let isRunning = false;
 
 cron.schedule(
-  "43 8 * * *",
+  "38 14 * * *",
   () => {
     console.log("Cron triggered at:", new Date().toLocaleTimeString());
 
