bc12eae432e9b631617f0cb6b1bb1c75a1cb2425
Shish <shishfruitwala29@gmail.com>
2026-04-29 22:48:02 +0530

    feat(api): daily fetch trial

--- server/cron/updatePrices.cron.js
+++ server/cron/updatePrices.cron.js
@@ -5,42 +5,37 @@ let pollInterval = null;
 let isRunning = false;
 
 cron.schedule(
-  "03 15 * * *",
+  "44 22 * * *",
   () => {
     console.log("Cron triggered at:", new Date().toLocaleTimeString());
 
     if (pollInterval) return;
 
-    pollInterval = setInterval(
-      async () => {
-        if (isRunning) return;
-        isRunning = true;
+    pollInterval = setInterval(async () => {
+      if (isRunning) return;
+      isRunning = true;
 
-        console.log(
-          "Polling API for today's data at",
-          new Date().toLocaleTimeString(),
-        );
+      console.log(
+        "Polling API for today's data at",
+        new Date().toLocaleTimeString(),
+      );
 
-        try {
-          const success = await fetchAndStoreDataForCron();
+      try {
+        const success = await fetchAndStoreDataForCron();
 
-          if (success) {
-            console.log("Today's mandi data fetched and added to DB!");
-            clearInterval(pollInterval);
-            pollInterval = null;
-          } else {
-            console.log(
-              "Today's data not posted by the govt. yet. Retrying...",
-            );
-          }
-        } catch (err) {
-          console.error("Error during polling:", err.message);
-        } finally {
-          isRunning = false;
+        if (success) {
+          console.log("Today's mandi data fetched and added to DB!");
+          clearInterval(pollInterval);
+          pollInterval = null;
+        } else {
+          console.log("Today's data not posted by the govt. yet. Retrying...");
         }
-      },
-      1 * 60 * 1000,
-    );
+      } catch (err) {
+        console.error("Error during polling:", err.message);
+      } finally {
+        isRunning = false;
+      }
+    }, 60 * 1000);
   },
   { timezone: "Asia/Kolkata" },
 );
