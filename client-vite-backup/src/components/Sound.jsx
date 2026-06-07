9f806f203303de8cf76972038898cfc22a33bc54
Shish <shishfruitwala29@gmail.com>
2026-06-07 13:12:14 +0530

    chore: remove migration backups

--- client-vite-backup/src/components/Sound.jsx
+++
@@ -1,6 +0,0 @@
-import bookmark from "../audio/added.mp3";
-import unbookmark from "../audio/notification.mp3";
-
-export const bookmarkSound = new Audio(bookmark);
-export const compareSound = new Audio(bookmark);
-export const unBookmarkSound = new Audio(unbookmark);
